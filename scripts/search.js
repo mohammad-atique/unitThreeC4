3640e793919058a7c5925d7888eead1068380b77
mohammad-atique <92262714+mohammad-atique@users.noreply.github.com>
2022-02-21 16:49:37 +0530

    added search and news code

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,13 @@
-function storeSearchterm(term) {
+async function storeSearchterm(term) {
 
+    try{
+        let res = await fetch(term);
+        let data = await res.json();
+        return data;
+    }
+    catch(err){
+        console.log(err)
+    }
 
 
 }
