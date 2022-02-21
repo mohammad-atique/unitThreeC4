519bb450ae12b454028e67ffc3438a28dda800b3
mohammad-atique <92262714+mohammad-atique@users.noreply.github.com>
2022-02-21 16:07:08 +0530

    added pages and code

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,7 +1,13 @@
 function sidebar() {
 
-
+return `<h1>Masai New</h1>
+<div >Login</div>
+<input type="text" id="searchbar" placeholder="Search News">
+<div >Startups</div>
+<div >News Latters</div>
+<div >audio</div>
+<div >Video</div>`
     // return your html component here
     //Make sure to give input search box id as ""
 }
-export default sidebar
+export default sidebar;
--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,14 @@ async function apiCall(url) {
 
 
     //add api call logic here
-
+    try{
+        let res= await fetch(url);
+        let data= await res.json();
+        return data;
+    }
+    catch(err){
+        console.log(err);
+    }
 
 }
 
@@ -10,6 +17,24 @@ async function apiCall(url) {
 function appendArticles(articles, main) {
 
     //add append logic here
+
+        articles.map((el)=>{
+            let div = document.createElement("div");
+            
+            div.addEventListener("click",()=>{
+                localStorage.setItem("article",JSON.stringify(el));
+                window.location.href="news.html"
+              })
+            let img = document.createElement("img");
+            img.src=el.urlToImage;
+            let title = document.createElement("p");
+            title.innerText=el.title;
+            let desc = document.createElement("p");
+            desc.innerText=el.description;
+            div.append(title,desc,img)
+            main.append(div)
+        });
+
 
 }
 
